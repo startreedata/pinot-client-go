3ce8db8bb7b122eb446bd33d465645696a15694f
troywinter24 <mengyuhu@whale.im>
2021-02-26 17:48:19 +0800

    fix: connection_test

--- pinot-client-go/connection_test.go
+++ pinot-client-go/connection_test.go
@@ -1,6 +1,7 @@
 package pinot
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -33,8 +34,8 @@ func TestSendingSQLWithMockServer(t *testing.T) {
 	assert.Equal(t, 1, resp.ResultTable.GetColumnCount())
 	assert.Equal(t, "cnt", resp.ResultTable.GetColumnName(0))
 	assert.Equal(t, "LONG", resp.ResultTable.GetColumnDataType(0))
-	assert.Equal(t, float64(97889), resp.ResultTable.Get(0, 0))
-	assert.Equal(t, 97889, resp.ResultTable.GetInt(0, 0))
+	assert.Equal(t, json.Number("97889"), resp.ResultTable.Get(0, 0))
+	assert.Equal(t, int32(97889), resp.ResultTable.GetInt(0, 0))
 	assert.Equal(t, int64(97889), resp.ResultTable.GetLong(0, 0))
 	assert.Equal(t, float32(97889), resp.ResultTable.GetFloat(0, 0))
 	assert.Equal(t, float64(97889), resp.ResultTable.GetDouble(0, 0))
